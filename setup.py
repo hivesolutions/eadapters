7cbaf677c67593e3bd8775810fb27528c28f0bd5
joamag <joamag@hive.pt>
2016-08-30 16:01:30 +0100

    new version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 
 setuptools.setup(
     name = "eadapters",
-    version = "0.2.13",
+    version = "0.3.0",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "E-Commerce Adapters",
